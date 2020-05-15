c9dae115bcbac899a4524c74a3f9b757908055ed
Rustam Sarin <lorienas@gmail.com>
2020-05-15 20:01:20 +0300

    realize download data from server

--- src/main.js
+++ src/main.js
@@ -1,42 +1,39 @@
 
-import Menu, {MenuItem} from '@components/menu.js';
-import FilterController from '@src/controllers/filter.js';
+import API from '@src/api.js';
 import Board from '@components/board.js';
-// import {generateFilters} from '@components/mock/filter.js';
-import {generateCards} from '@components/mock/card.js';
-import {render, RenderPosition} from '@src/utils/render.js';
 import BoardController from '@src/controllers/board.js';
 import CardsModel from '@src/models/cardsModel.js';
+import FilterController from '@src/controllers/filter.js';
+import Menu, {MenuItem} from '@components/menu.js';
 import Statistics from '@components/statistics.js';
 
+import {render, RenderPosition} from '@src/utils/render.js';
+
+const AUTHORIZATION = `Basic AsdfDSGSdgdsgdsgs=`;
+
 const init = function () {
-  const cardsData = generateCards();
-  const cardsModel = new CardsModel();
-  cardsModel.setCards(cardsData);
-
-  const siteMain = document.querySelector(`.main`);
-  const siteMainSection = siteMain.querySelector(`.main__control`);
-
-  const menu = new Menu();
-  render(siteMainSection, menu, RenderPosition.BEFOREEND);
-
-  const filterController = new FilterController(siteMain, cardsModel);
-  filterController.render();
-
-  const board = new Board();
-  render(siteMain, board, RenderPosition.BEFOREEND);
-
-  const boardController = new BoardController(board, cardsModel);
-
-  boardController.render(cardsData);
-
   const dateTo = new Date();
   const dateFrom = (() => {
     const d = new Date(dateTo);
     d.setDate(d.getDate() - 7);
     return d;
   })();
+
+  const api = new API(AUTHORIZATION);
+  const cardsModel = new CardsModel();
+
+  const siteMain = document.querySelector(`.main`);
+  const siteMainSection = siteMain.querySelector(`.main__control`);
+
+  const menu = new Menu();
   const statisticsComponent = new Statistics({tasks: cardsModel, dateFrom, dateTo});
+  const board = new Board();
+  const boardController = new BoardController(board, cardsModel);
+  const filterController = new FilterController(siteMain, cardsModel);
+
+  render(siteMainSection, menu, RenderPosition.BEFOREEND);
+  filterController.render();
+  render(siteMain, board, RenderPosition.BEFOREEND);
   render(siteMainSection, statisticsComponent, RenderPosition.BEFOREEND);
   statisticsComponent.hide();
 
@@ -58,6 +55,12 @@ const init = function () {
         break;
     }
   });
+
+  api.getCards()
+    .then((cards) => {
+      cardsModel.setCards(cards);
+      boardController.render();
+    });
 };
 
 init();
---
+++ src/models/card.js
@@ -0,0 +1,19 @@
+export default class Card {
+  constructor(data) {
+    this.id = data[`id`];
+    this.description = data[`description`] || ``;
+    this.dueDate = data[`due_date`] ? new Date(data[`due_date`]) : null;
+    this.repeatingDays = data[`repeating_days`];
+    this.color = data[`color`];
+    this.isFavorite = Boolean(data[`is_favorite`]);
+    this.isArchive = Boolean(data[`is_archived`]);
+  }
+
+  static parseCard(data) {
+    return new Card(data);
+  }
+
+  static parseCards(data) {
+    return data.map(Card.parseCard);
+  }
+}
